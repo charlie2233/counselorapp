cb6fdb74a30b644a1e2c466cefdd3f75859c4069
CharLEE.cpp <changlin9696@gmail.com>
2024-06-20 16:02:45 -0700

    test

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -11,6 +11,19 @@ class home_screen extends StatefulWidget {
 class _home_screenState extends State<home_screen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            children: [
+              Text("Home Counselor",style: TextStyle(fontSize: 40),),
+              ElevatedButton(onPressed: (){}, child: Text("Login",style: TextStyle(fontSize: 40),),style: ElevatedButton.styleFrom(backgroundColor: Colors.blue,),),
+              ElevatedButton(onPressed: (){}, child: Text("Register",style: TextStyle(fontSize: 40),),style: ElevatedButton.styleFrom(backgroundColor: Colors.yellow,),),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
